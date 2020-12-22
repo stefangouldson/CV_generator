1e0161c4c6363210b919dd081ef1db6adc39373c
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-22 15:35:04 +0000

    view added + alter max_length

--- pdf/models.py
+++ pdf/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=2000)
     degree = models.CharField(max_length=200)
     school = models.CharField(max_length=200)
     university = models.CharField(max_length=200)
