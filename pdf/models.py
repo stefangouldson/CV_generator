1cd44a6f8e1beccbb2d845e63de94fb912795df9
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-22 20:59:03 +0000

    return name on admin

--- pdf/models.py
+++ pdf/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
 
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
